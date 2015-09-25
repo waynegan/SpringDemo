0daded6f9315ed3246f9dec99e044283c53fdd61
waynegan <ganwei92@gmail.com>
2015-09-25 01:33:18 -0400

    Writing code using bean factory

---
+++ src/gq/bithub/wayne/springDemo/DrawingApp.java
@@ -0,0 +1,12 @@
+package gq.bithub.wayne.springDemo;
+
+/**
+ * @author wayne
+ *
+ */
+public class DrawingApp {
+	public static void main(String[] args) {
+		Triangle triangle = new Triangle();
+		triangle.draw();
+	}
+}
---
+++ src/gq/bithub/wayne/springDemo/Triangle.java
@@ -0,0 +1,8 @@
+package gq.bithub.wayne.springDemo;
+
+public class Triangle {
+public void draw(){
+	System.out.println("Triangle drawn.");
+	
+}
+}
