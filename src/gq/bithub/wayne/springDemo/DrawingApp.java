01942cbbca92fafb222d8fa19198ba10b545bc91
waynegan <ganwei92@gmail.com>
2015-09-25 16:21:01 -0400

    ApplicationContext

--- src/gq/bithub/wayne/springDemo/DrawingApp.java
+++ src/gq/bithub/wayne/springDemo/DrawingApp.java
@@ -1,12 +1,20 @@
 package gq.bithub.wayne.springDemo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author wayne
  *
  */
 public class DrawingApp {
+	private static ApplicationContext context;
+
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
-		triangle.draw();
+		// Triangle triangle = new Triangle();
+		context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle obj = (Triangle) context.getBean("Triangle");
+		obj.draw();
+
 	}
 }
