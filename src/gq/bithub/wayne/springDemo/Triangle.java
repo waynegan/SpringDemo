93087710a0e8fee304d672d39aa36666de89d006
waynegan <ganwei92@gmail.com>
2015-09-25 16:39:14 -0400

    Add property for bean

--- src/gq/bithub/wayne/springDemo/Triangle.java
+++ src/gq/bithub/wayne/springDemo/Triangle.java
@@ -1,8 +1,22 @@
 package gq.bithub.wayne.springDemo;
 
+/**
+ * @author wayne
+ *
+ */
 public class Triangle {
-public void draw(){
-	System.out.println("Triangle drawn.");
-	
-}
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println(getType() +  " Triangle drawn.");
+
+	}
 }
