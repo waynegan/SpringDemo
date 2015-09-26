9f7c95daeb7e0367b7f9243228ff367f9c7cbf1e
waynegan <ganwei92@gmail.com>
2015-09-25 21:13:32 -0400

    some ways to locate bean

--- src/gq/bithub/wayne/springDemo/Triangle.java
+++ src/gq/bithub/wayne/springDemo/Triangle.java
@@ -6,17 +6,30 @@ package gq.bithub.wayne.springDemo;
  */
 public class Triangle {
 	private String type;
+	private int height;
+	public int getHeight() {
+		return height;
+	}
 
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(int height) {
+		this.height = height;
+	}
+
+
+	
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
+	/*
+	 * public void setType(String type) { this.type = type; }
+	 */
 	public void draw() {
-		System.out.println(getType() +  " Triangle drawn.");
+		System.out.println(getType() + " Triangle drawn." + getHeight());
 
 	}
 }
