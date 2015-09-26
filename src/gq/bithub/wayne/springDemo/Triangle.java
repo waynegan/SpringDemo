038c3ed75f0f177a34c26b96239680e8c9ec0803
waynegan <ganwei92@gmail.com>
2015-09-25 21:58:53 -0400

    use beans to implement pointLocation

--- src/gq/bithub/wayne/springDemo/Triangle.java
+++ src/gq/bithub/wayne/springDemo/Triangle.java
@@ -5,31 +5,44 @@ package gq.bithub.wayne.springDemo;
  *
  */
 public class Triangle {
-	private String type;
-	private int height;
-	public int getHeight() {
-		return height;
-	}
-
-	public Triangle(String type) {
-		this.type = type;
-	}
-
-	public Triangle(int height) {
-		this.height = height;
+	private point pointA;
+	private point pointB;
+	public point getPointA() {
+		return pointA;
 	}
 
 
+	public void setPointA(point pointA) {
+		this.pointA = pointA;
+	}
+
+
+	public point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	private point pointC;
 	
-	public String getType() {
-		return type;
-	}
-
-	/*
-	 * public void setType(String type) { this.type = type; }
-	 */
+	
 	public void draw() {
-		System.out.println(getType() + " Triangle drawn." + getHeight());
-
+		System.out.println("pointA = ( "+ getPointA().getX() + "," + getPointA().getY()+")");
+		System.out.println("pointB = ( "+ getPointB().getX() + "," + getPointB().getY()+")");
+		System.out.println("pointC = ( "+ getPointC().getX() + "," + getPointC().getY()+")");
 	}
 }
---
+++ src/gq/bithub/wayne/springDemo/point.java
@@ -0,0 +1,18 @@
+package gq.bithub.wayne.springDemo;
+
+public class point {
+	private int x;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	private int y;
+}
