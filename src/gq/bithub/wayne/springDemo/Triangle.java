d2581a378fc9274fb87ef63b8313dd01e926c140
waynegan <ganwei92@gmail.com>
2015-09-27 01:13:12 -0400

    setApplicationContext and beanName

--- src/gq/bithub/wayne/springDemo/Triangle.java
+++ src/gq/bithub/wayne/springDemo/Triangle.java
@@ -1,28 +1,60 @@
 package gq.bithub.wayne.springDemo;
 
-import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 /**
  * @author wayne
  *
  */
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	private List<Point> points;
+	private ApplicationContext context = null;
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		for (Point point : points) {
-			System.out.println("point A = (" + point.getX() + " ," + point.getY() + ")");
+		System.out.println("pointA = ( " + getPointA().getX() + "," + getPointA().getY() + ")");
+		System.out.println("pointB = ( " + getPointB().getX() + "," + getPointB().getY() + ")");
+		System.out.println("pointC = ( " + getPointC().getX() + "," + getPointC().getY() + ")");
+	}
 
-		}
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;
+
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		System.out.println("Bean name is :" + beanName);
 
 	}
 }
